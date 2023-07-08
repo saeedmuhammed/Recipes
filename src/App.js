651b224d5e9e175c433c91688d4ee48b6f92e8f5
saeedmuhammed <saidmsaid2011@gmail.com>
2023-07-08 18:31:22 +0300

    make the routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// pages import
+import Home from './Pages/home/Home';
+import Create from './Pages/create/Create'
+import Search from './Pages/search/Search';
+import Recipe from './Pages/recipe/Recipe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      
+        <Route path='/create' element={<Create />}/>
+          
+        <Route path='/search' element={<Search />}/>
+            
+        
+        <Route path='/recipe/:id' element={<Recipe />}/>
+
+        <Route path='*' element={<Navigate to='/' />} />
+
+      </Routes>
+
+       </div>
   );
 }
 
